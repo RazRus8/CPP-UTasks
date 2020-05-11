939ae791c9f76828cec2f78d48570411a63e9293
Ruslan Razuvailo <razrus@hotmail.com>
2020-05-11 15:47:43 +0300

    Error correction.

--- Lesson6/ControlTasks/ControlTask1/ControlTask1.cpp
+++ Lesson6/ControlTasks/ControlTask1/ControlTask1.cpp
@@ -9,21 +9,27 @@ using namespace std;
 int main()
 {
 	string poem;
-
-	cout << "Enter a poem: ";
-	getline(cin, poem);
+	int lines;
+	cout << "How many lines does poem contain?: ";
+	cin >> lines;
+	cout << "Enter a poem in " << lines << " lines:\n";
 
 	ofstream out ("Poem.txt", ofstream::out);
-
-	if (!out)
+	
+	for (int i = 0; i <= lines; i++)
 	{
-		cout << "Unable to open a file\n";
-		return 1;
-	}
+		getline(cin, poem);
+		
+		if (!out)
+		{
+			cout << "Unable to open a file\n";
+			return 1;
+		}
 
-	if (out.is_open())
-	{
-		out << poem;
+		if (out.is_open())
+		{
+			out << poem << "\n";
+		}
 	}
 
 	cout << "The poem has successfuly written into the file\n";
